c1ea4a947a88e138a94d67a30efb983e651e2a65
Kekkles1 <fatimamahmood13@outlook.com>
2025-05-15 00:23:27 +0500

    Added basic animation to player

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var sprite = $AnimatedSprite2D
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
+	
+	if input_direction != Vector2.ZERO:
+		sprite.play("walk")
+		
+		if input_direction.x != 0:
+			sprite.flip_h = input_direction.x < 0  # flip left if moving left
+	else:
+		sprite.stop()
 
 func _physics_process(_delta):
 	get_input()
